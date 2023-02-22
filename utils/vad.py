c8b8d4c2c2c9e13712a6cf3c2af46f1f9e6fbd1b
MrEnigmamgine "Joshua Mayes <mayes.joshua.d@gmail.com>
2023-02-21 21:04:20 -0500

    minor changes to VAD and requirements

--- utils/vad.py
+++ utils/vad.py
@@ -8,13 +8,14 @@ models_dir = './models'
 
 class VAD():
     """Wrapper class to ease the making and using of the silero-vad model."""
-    def __init__(self):
+    def __init__(self, force_reload=False):
         ## Silero VAD Model
         silero_dir = os.path.join(models_dir, 'silero-vad')
-        vad_model, utils =    torch.hub.load(repo_or_dir=silero_dir,
+        source_str = silero_dir if os.path.exists(silero_dir) else 'snakers4/silero-vad'
+        vad_model, utils =    torch.hub.load(repo_or_dir=source_str,
                                     source='local',
                                     model='silero_vad',
-                                    force_reload=True,
+                                    force_reload=force_reload,
                                     onnx=False)
 
         self._model = vad_model
